67b0529c16bd9e90b9b760f4fa52771002d5f4c6
abloberdiev shoislom <abloberdiev.shm@phystech.edu>
2023-11-16 23:44:42 +0300

    removed some files

--- text_editor/src/theme_operations.py
+++
@@ -1,6 +0,0 @@
-class ThemeOperations:
-    def __init__(self, text_editor):
-        self.text_editor = text_editor
-
-    def change_theme(self, bg_color, fg_color):
-        self.text_editor.text_editor.config(bg=bg_color, fg=fg_color)
